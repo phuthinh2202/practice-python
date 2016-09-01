717d7946b32132bfa72fc832c5ad108552ff16c3
root <root@puppetagent.example.com>
2016-09-01 08:08:09 +0700

    add file fabfile

---
+++ fabfile.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# Description: Fabric practice 
+# Author: Lis
+from fabric.api import *
+
+env.hosts = ['222.255.239.65']
+env.user = 'root'
+env.port = 7878
+#env.use_ssh_config = True
+env.key_filename = ['/home/keys/prikey.pem']
+
+def ls():
+    run('ls -l')
